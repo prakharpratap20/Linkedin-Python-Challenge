37c8b82391533ff41a8385f7e89c16844b566113
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-08-03 15:19:24 +0530

    1

---
+++ findPrimeFactor.py
@@ -0,0 +1,15 @@
+# Challenge
+# Write a Python function to find all prime factors
+
+def get_prime_factors(number):
+    factors = []
+    divisor = 2
+    while divisor <= number:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number = number // divisor
+        else:
+            divisor += 1
+    return factors
+
+print(get_prime_factors(630))
