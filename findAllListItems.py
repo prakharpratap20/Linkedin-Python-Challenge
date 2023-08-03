b5a7547d68934a1bf89efb02c0464b40e961dddd
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-08-03 23:03:04 +0530

    4

--- findAllListItems.py
+++ findAllListItems.py
@@ -13,4 +13,4 @@ def index_all(search_list, item):
 
 example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
 print(index_all(example, 2))
-print(index_all(example, [1, 2, 3]))
+print(index_all(example, [1, 2, 3])) 
