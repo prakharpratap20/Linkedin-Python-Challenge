cf354622aec7b096e8e176d7518af164f75b24f5
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-08-04 14:37:46 +0530

    8

---
+++ diceSimulator.py
@@ -0,0 +1,17 @@
+# Challenge 
+# Write a Python function to determine the probability of 
+# certain outcomes when rolling dice
+
+from random import randint  
+from collections import Counter
+
+def rollDice(*dice, numTrials=1000000):
+    counts = Counter()
+    for _ in range(numTrials):
+        counts[sum((randint(1, sides) for sides in dice))] += 1
+        
+    print("\n Outcome \t Probability")
+    for outcome in range(len(dice), sum(dice) + 1):
+        print(f"{outcome}\t{counts[outcome] * 100 / numTrials :0.2f}%")
+        
+rollDice(2)
